87f04fe2148ce4d7e3149ee1aed4eb3e0aa7cd0b
smeh09 <shauryameh09@gmail.com>
2021-08-18 13:28:08 +0530

    Made the to do list input responsive and fixed
    Now the padding is fixed and the width error in the input boxes are fixed. It is also responsive now.

--- apps/to-do-list/JS/to-do-list.js
+++ apps/to-do-list/JS/to-do-list.js
@@ -1,10 +1,15 @@
 function createTask() {
-    var noTask = document.querySelector('#no-tasks');
     var taskInput = document.querySelector('#create-task-input');
+    var noTaskHeading = document.querySelector('#no-tasks');
     var task = taskInput.value;
+    var firstTask = false;
+    tasks = []
+
     if(task != '') {
-        noTask.style.display = 'none';
-        console.log(task);
+        if(noTaskHeading.style.display != 'none') {
+            noTaskHeading.style.display = 'none';
+        }
+        taskInput.value = '';
     }
     else {
         alert('No task selected! ');
