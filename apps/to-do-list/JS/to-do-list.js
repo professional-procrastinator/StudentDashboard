7230c2e8ece30fd4562ae4b7509ae96666f1717d
smeh09 <shauryameh09@gmail.com>
2021-08-18 14:02:53 +0530

    Rendering of earlier tasks done.
    Now this javascript gets the data from the local storage and renders it. But it doesn't has add or remove or done features yet. Have to work on that.

--- apps/to-do-list/JS/to-do-list.js
+++ apps/to-do-list/JS/to-do-list.js
@@ -1,3 +1,39 @@
+localStorage.setItem('tasks', ['Do homework', 'Play', 'Complete To do list']);
+
+function loadTasks() {
+    if(localStorage.getItem('tasks') == null) {
+        return []
+    }
+    else {
+        return localStorage.getItem('tasks').split(',');
+    }
+}
+
+function renderTask(task, tasksDiv) {
+    var taskDiv = document.createElement('div');
+    taskDiv.classList.add('task')
+    taskDiv.innerHTML = task;
+    tasksDiv.appendChild(taskDiv);
+}
+
+function renderTasks(tasks) {
+    var tasksDiv = document.createElement('div');
+    var noTaskHeading = document.querySelector('#no-tasks');
+    tasksDiv.id = 'tasks-container';
+
+    var createTaskDiv = document.querySelector('#create-task-div');
+    
+    if(tasks.length != 0) {
+        noTaskHeading.style.display = 'none';
+    }
+
+    tasks.forEach((task, index) => {
+        renderTask(task, tasksDiv);
+    });
+
+    createTaskDiv.insertAdjacentElement('beforebegin', tasksDiv)
+}
+
 function createTask() {
     var taskInput = document.querySelector('#create-task-input');
     var noTaskHeading = document.querySelector('#no-tasks');
@@ -15,3 +51,6 @@ function createTask() {
         alert('No task selected! ');
     }
 }
+
+tasks = loadTasks();
+renderTasks(tasks);
