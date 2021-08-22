8b0a117c8a0d2540d58fe26923c3680c2d9a6a19
smeh09 <shauryameh09@gmail.com>
2021-08-22 16:57:08 +0530

    Storage funciton
    Now the tasks the users add, are stored in local storage and they also get loaded and rendered when people load the website again.

--- apps/to-do-list/JS/to-do-list.js
+++ apps/to-do-list/JS/to-do-list.js
@@ -5,11 +5,66 @@ const clearTasksButton = document.querySelector('#clear-tasks-button');
 const taskInput = document.querySelector('#create-task-input');
 const createTaskDiv = document.querySelector('#create-task-div');
 
-// Create a tasks list
-let tasks = [];
-
 // Make my localstorage here
 const taskStorage = window.localStorage;
+
+// Load tasks
+function loadTasks() {
+    let tasks1 = taskStorage.getItem('tasks');
+    if(tasks1 == null) {
+        return [];
+    }
+    else if(tasks1 == '') {
+        return [];
+    }
+    else {
+        return tasks1.split(',');
+    }
+}
+
+function renderFirstTask(task) {
+    let tasksContainer = document.createElement('div');
+    tasksContainer.id = 'tasks-container';
+    
+    let taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    let taskCheckMark = document.createElement('div');
+    taskCheckMark.classList.add('task-check-mark');
+
+    let taskData = document.createElement('div');
+    taskData.classList.add('task-data');
+
+    let taskRemove = document.createElement('div');
+    taskRemove.classList.add('task-remove');
+
+    // Structure the elements
+    taskRemove.innerText = '🗑'
+    taskRemove.title = 'Trash';
+
+    taskData.innerText = `${task}`;
+    taskData.title = `${task}`;
+
+    taskCheckMark.innerText = '✔';
+    taskCheckMark.title = 'Finish Task';
+
+    taskContainer.appendChild(taskCheckMark);
+    taskContainer.appendChild(taskData);
+    taskContainer.appendChild(taskRemove);
+
+    tasksContainer.appendChild(taskContainer);
+
+    // Render the elements
+    createTaskDiv.insertAdjacentElement('beforebegin', tasksContainer);
+}
+
+// Create a tasks list
+let tasks = loadTasks();
+if(tasks.length != 0) {
+    tasks.forEach((task, index) => {
+        renderFirstTask(task);
+    })
+}
 
 // Update local storage
 function updateStorage(tasks) {
@@ -19,40 +74,7 @@ function updateStorage(tasks) {
 // Render task
 function renderTask(task, tasks) {
     if(tasks.length == 1) {
-        // Make elements
-        let tasksContainer = document.createElement('div');
-        tasksContainer.id = 'tasks-container';
-        
-        let taskContainer = document.createElement('div');
-        taskContainer.classList.add('task-container');
-
-        let taskCheckMark = document.createElement('div');
-        taskCheckMark.classList.add('task-check-mark');
-
-        let taskData = document.createElement('div');
-        taskData.classList.add('task-data');
-
-        let taskRemove = document.createElement('div');
-        taskRemove.classList.add('task-remove');
-
-        // Structure the elements
-        taskRemove.innerText = '🗑'
-        taskRemove.title = 'Trash';
-
-        taskData.innerText = `${task}`;
-        taskData.title = `${task}`;
-
-        taskCheckMark.innerText = '✔';
-        taskCheckMark.title = 'Finish Task';
-
-        taskContainer.appendChild(taskCheckMark);
-        taskContainer.appendChild(taskData);
-        taskContainer.appendChild(taskRemove);
-
-        tasksContainer.appendChild(taskContainer);
-
-        // Render the elements
-        createTaskDiv.insertAdjacentElement('beforebegin', tasksContainer);
+        renderFirstTask(task);
     }
     else {
         let tasksContainer = document.querySelector('#tasks-container')
@@ -102,10 +124,11 @@ function createTask(task) {
     }
 }
 
-// Get tasks
+// Get and Create tasks
 function getAndCreateTask() {
     const task = taskInput.value;
     createTask(task);
+    taskInput.value = '';
 }
 
 // Clear tasks
