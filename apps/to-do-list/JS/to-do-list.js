b884c221729eb43ad69e48dfdb26e3b700253b1d
smeh09 <shauryameh09@gmail.com>
2021-08-25 17:31:44 +0530

    Made the delete function

--- apps/to-do-list/JS/to-do-list.js
+++ apps/to-do-list/JS/to-do-list.js
@@ -1,8 +1,8 @@
 const taskForm = document.querySelector('.to-do-form');
 const list = document.querySelector('.list');
 
-// Array for tasks
-let tasks = [];
+// Array for items
+let items = [];
 
 function handleSubmit(event) {
     event.preventDefault();
@@ -16,22 +16,49 @@ function handleSubmit(event) {
         complete: false,
     };
     // Push the items into our array
-    tasks.push(item);
+    items.push(item);
     // Clear the form
     event.target.reset();
-    displayItems();
+    // fire off a custom event
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
 }
 
 function displayItems() {
-    const html = tasks
+    const html = items
         .map(
             item => `<li class="task-item">
             <input type='checkbox'>
             <span class='itemName'>${item.name}</span>
-            <button aria-label='Remove ${item.name}'>&times;</button>
+            <button class='remove-buttons' value='${item.id}' aria-label='Remove ${item.name}'>&times;</button>
         </li>`
         ).join('')
     list.innerHTML = html;
 }
 
+function updateLocalStorage() {
+    localStorage.setItem('items', JSON.stringify(items));
+}
+
+function restoreFromLocalStorage() {
+    const lsItems = JSON.parse(localStorage.getItem('items'));
+    if(lsItems == null) return;
+    if(lsItems.length) {
+        items = lsItems;
+        list.dispatchEvent(new CustomEvent('itemsUpdated'));
+    }
+}
+
+function deleteItem(id) {
+    items = items.filter(item => item.id !== id);
+    list.dispatchEvent(new CustomEvent('itemsUpdated'));
+}
+
 taskForm.addEventListener('submit', handleSubmit);
+list.addEventListener('itemsUpdated', displayItems);
+list.addEventListener('itemsUpdated', updateLocalStorage);
+list.addEventListener('click', function(e) {
+    if(e.target.matches('.remove-buttons')) {
+        deleteItem(parseInt(e.target.value));
+    }
+})
+restoreFromLocalStorage();
