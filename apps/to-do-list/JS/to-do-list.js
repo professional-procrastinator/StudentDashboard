3e64fe3742eaaff026cffc67db86ef65cf33dae8
smeh09 <shauryameh09@gmail.com>
2021-08-25 14:18:05 +0530

    Temp adding feature

--- apps/to-do-list/JS/to-do-list.js
+++ apps/to-do-list/JS/to-do-list.js
@@ -1,1 +1,37 @@
-//const taskForm = document.querySelector('.')
+const taskForm = document.querySelector('.to-do-form');
+const list = document.querySelector('.list');
+
+// Array for tasks
+let tasks = [];
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.currentTarget.item.value;
+    
+    if (!name) return;
+
+    const item = {
+        name,
+        id: Date.now(),
+        complete: false,
+    };
+    // Push the items into our array
+    tasks.push(item);
+    // Clear the form
+    event.target.reset();
+    displayItems();
+}
+
+function displayItems() {
+    const html = tasks
+        .map(
+            item => `<li class="task-item">
+            <input type='checkbox'>
+            <span class='itemName'>${item.name}</span>
+            <button aria-label='Remove ${item.name}'>&times;</button>
+        </li>`
+        ).join('')
+    list.innerHTML = html;
+}
+
+taskForm.addEventListener('submit', handleSubmit);
