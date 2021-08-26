ff7289884c97ec58f190cdbb5d461f75ccd38ce7
smeh09 <shauryameh09@gmail.com>
2021-08-26 14:22:52 +0530

    Clear function added and break bug removed

--- apps/paint/JS/paint.js
+++ apps/paint/JS/paint.js
@@ -4,24 +4,35 @@ const increaseBtn = document.querySelector('#increase');
 const decreaseBtn = document.querySelector('#decrease');
 const displayingSize = document.querySelector('#size');
 const colorEl = document.querySelector('#color');
+const clearEl = document.querySelector('#clear');
 
 let size = 15;
 let isPressed = false;
 let color = 'black';
+let x;
+let y;
 
-canvas.addEventListener('mousedown', () => {
+canvas.addEventListener('mousedown', (e) => {
     isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', () => {
+canvas.addEventListener('mouseup', (e) => {
     isPressed = false;
+
+    x = y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e) => {
     if(isPressed) {
-        const x = e.offsetX;
-        const y = e.offsetY;
-        drawCircle(x, y);
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+        //drawCircle(x2, y2)
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     }
 });
 
@@ -30,6 +41,17 @@ function drawCircle(x, y) {
     ctx.arc(x, y, size, 0, 2 * Math.PI);
     ctx.fillStyle = color;
     ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
 }
 
 increaseBtn.addEventListener('click', () => {
@@ -48,6 +70,10 @@ decreaseBtn.addEventListener('click', () => {
 
 colorEl.addEventListener('change', (e) => {
     color = e.target.value;
+});
+
+clearEl.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 })
 
 function updateSizeOnDisplay() {
